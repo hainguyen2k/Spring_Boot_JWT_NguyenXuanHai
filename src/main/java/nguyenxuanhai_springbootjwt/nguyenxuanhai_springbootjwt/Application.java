a5c6f8f0a5ef33305f7d8005c534392783e5a9bc
hainguyen2k <ngxuanhai1000@gmail.com>
2021-09-24 08:44:14 +0700

    Add Entity

---
+++ src/main/java/nguyenxuanhai_springbootjwt/nguyenxuanhai_springbootjwt/Application.java
@@ -0,0 +1,13 @@
+package nguyenxuanhai_springbootjwt.nguyenxuanhai_springbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
